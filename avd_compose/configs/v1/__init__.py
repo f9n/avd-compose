ab4fdef36b3967de1f35e2d0c1a88b5f9d2b85f9
Fatih Sarhan <f9n@protonmail.com>
2019-03-15 15:43:32 +0300

    Remove unnecessary function that checks avd fields of the platform

--- avd_compose/configs/v1/__init__.py
+++ avd_compose/configs/v1/__init__.py
@@ -1,24 +1,13 @@
 import sys
 
 
-def check_missing_keys_in_dictionary(dictionary, required_fields):
-    missing_fields = []
-    for required_field in required_fields:
-        if not dictionary.get(required_field):
-            missing_fields.append(required_field)
-
-    return missing_fields
-
-
 def validate(configs):
     messages = []
     avd_required_fields = ["package", "device"]
     for platform in configs["platforms"]:
         name = platform["name"]
-        # Check avd field
-        missing_fields = check_missing_keys_in_dictionary(
-            platform["avd"], avd_required_fields
-        )
+        # Check platform avd field
+        missing_fields = avd_required_fields - platform["avd"].keys()
         if missing_fields:
             messages.append(
                 "Missing fields in avd section of '{name}' platform:\n{fields}".format(
