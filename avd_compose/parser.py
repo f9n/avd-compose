e004896fea01d2d90a844c590adb67d4a7feb2c2
Fatih Sarhan <f9n@protonmail.com>
2019-03-15 13:59:44 +0300

    Add basic validation function for configuration file

--- avd_compose/parser.py
+++ avd_compose/parser.py
@@ -3,13 +3,41 @@ import sys
 from .utils import yaml
 
 
-def validate():
-    pass
+def check_missing_keys_in_dictionary(dictionary, required_fields):
+    missing_fields = []
+    for required_field in required_fields:
+        if not dictionary.get(required_field):
+            missing_fields.append(required_field)
+
+    return missing_fields
 
 
+# TODO:: Refactor
+def validate_configuration_file(configs):
+    messages = []
+    avd_required_fields = ["package", "device"]
+    for platform in configs["platforms"]:
+        name = platform["name"]
+        # Check avd field
+        missing_fields = check_missing_keys_in_dictionary(
+            platform["avd"], avd_required_fields
+        )
+        if missing_fields:
+            messages.append(
+                "Missing fields in avd section of '{name}' platform:\n{fields}".format(
+                    name=name, fields=", ".join(missing_fields)
+                )
+            )
+
+    if messages:
+        sys.exit("\n".join(messages))
+
+
+# TODO:: Refactor
 def parse_configuration_file(config_filepath):
     try:
         content = yaml.load_file(config_filepath)
+        validate_configuration_file(content)
         return content
     except Exception as e:
         sys.exit(e)
---
+++ avd_compose/utils/formatter.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def options_as_a_string(options):
+    options_string = ""
+    for k, v in options.items():
+        if type(v) == str:
+            option = "--{option_name} '{option_value}' ".format(
+                option_name=k, option_value=v
+            )
+        elif type(v) == bool and v is True:
+            option = "--{option_name} ".format(option_name=k)
+        else:
+            sys.exit("We just support string and bool types for '{}' field".format(k))
+
+        options_string += option
+
+    return options_string
