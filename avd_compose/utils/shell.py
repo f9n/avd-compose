23b6e8b3c762bc3df027cc3dfd50608ec1e979e3
Fatih Sarhan <f9n@protonmail.com>
2019-03-16 18:29:44 +0300

    Exit when the internal command returns an error.

--- avd_compose/utils/shell.py
+++ avd_compose/utils/shell.py
@@ -6,5 +6,8 @@ import delegator
 def run_command(command):
     print("$ {command}".format(command=command))
     c = delegator.run(command)
+    if not c.err == 0:
+        sys.exit(c.err)
+
     return c.out, c.err, c.return_code
 
