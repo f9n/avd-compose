e12d7b01715826bae385550232c960503a6b19aa
Fatih Sarhan <f9n@protonmail.com>
2019-03-16 20:41:46 +0300

    Fix incorrect variable name on the 'run_command' of 'shell' module

--- avd_compose/utils/shell.py
+++ avd_compose/utils/shell.py
@@ -6,8 +6,7 @@ import delegator
 def run_command(command):
     print("$ {command}".format(command=command))
     c = delegator.run(command)
-    if not c.err == 0:
+    if not c.return_code == 0:
         sys.exit(c.err)
 
     return c.out, c.err, c.return_code
-
