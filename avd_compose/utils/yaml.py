fdc5eb8f31301b544aa2a3a879dd8774ec81dc55
Fatih Sarhan <f9n@protonmail.com>
2019-03-17 09:28:47 +0300

    Use the 'safe_load' function instead of the deprecated 'load' function

--- avd_compose/utils/yaml.py
+++ avd_compose/utils/yaml.py
@@ -7,7 +7,7 @@ import yaml as y
 def load_file(filepath):
     try:
         with io.open(filepath, "r") as f:
-            return y.load(f)
+            return y.safe_load(f)
     except Exception as e:
         sys.exit(e)
 
